d6a8a4886d3e82dad76d3087eec97ccc36280b55
nemetig <gergely.nemeti@gmail.com>
2018-12-01 22:32:12 +0100

    Initial check-in of module CSharpAlapok

---
+++ 01Bevezetes/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01Bevezetes
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Hello World!");
+            Console.WriteLine("Hello bello!");
+            Console.WriteLine("Hello bello!");
+            Console.ReadLine();
+           // Abrakadabra
+        }
+    }
+}
---
+++ 02Receptes/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _02Receptes
+{
+    /// <summary>
+    /// Legyen az a feladat, hogy egy pörkölt recept hozzávalóit kiírjuk.
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Marhahús");
+            Console.WriteLine("Vöröshagyma");
+            Console.WriteLine("Zsír");
+            Console.WriteLine("Pirospaprika");
+
+            Console.ReadLine();
+    
+        }
+    }
+}
